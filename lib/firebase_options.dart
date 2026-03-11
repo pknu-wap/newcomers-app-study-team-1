f0b9d20e1c55dc7507366f59d3608663ed1a175e
hide-squid <developer.tbk@gmail.com>
2026-03-11 22:01:33 +0900

    new api setting?

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -3,7 +3,7 @@
 import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
 import 'package:flutter/foundation.dart'
     show defaultTargetPlatform, kIsWeb, TargetPlatform;
-import 'api.dart';
+import 'package:flutter_clone_instagram/api.dart';
 
 /// Default [FirebaseOptions] for use with your Firebase apps.
 ///
